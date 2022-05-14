49872ac6f967f88f380847e5eefe8b24a8e93936
Kevim Volpini <kevim_volpini@hotmail.com>
2022-05-14 00:42:41 -0300

    add new features

---
+++ Assets/Scripts/Constantes.cs
@@ -0,0 +1,7 @@
+public class Constantes {
+    public const string TAG_JOGADOR = "Jogador";
+    public const string TAG_INIMIGO = "Inimigo";
+    public const string TAG_PONTUACAO = "Pontuacao";
+
+    public const string ANIMACAO_ATACANDO = "Atacando";
+}
