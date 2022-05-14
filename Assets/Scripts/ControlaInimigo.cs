4b2e01580a96797c6611606db3c9be8286788835
Kevim Volpini <kevim_volpini@hotmail.com>
2022-05-14 20:10:50 -0300

    refactory and add movimentacao ANDAR

--- Assets/Scripts/ControlaInimigo.cs
+++ Assets/Scripts/ControlaInimigo.cs
@@ -14,27 +14,21 @@ public class ControlaInimigo : MonoBehaviour
     public int DanoMin = 20;
     public int DanoMax = 30;
     public AudioClip SomMorte;
-
-    private Animator animatorInimigo;
-
-    private Rigidbody rigidbodyInimigo;
+    private AnimacaoPersonagem animacaoPersonagem;
     private Pontuacao pontuacao;
     private ControlaJogador controlaJogador;
+    private MovimentoPersonagem movimentoPersonagem;
 
     // Start is called before the first frame update
     void Start()
     {
+        this.animacaoPersonagem = GetComponent<AnimacaoPersonagem>();
+        this.movimentoPersonagem = GetComponent<MovimentoPersonagem>();
+
+        this.pontuacao = Pontuacao.instance;
         this.Jogador = GameObject.FindWithTag(Constantes.TAG_JOGADOR);
-        
         this.controlaJogador = this.Jogador.GetComponent<ControlaJogador>();
-
-        int randomZombieSkin = Random.Range(1, 28);
-        transform.GetChild(randomZombieSkin).gameObject.SetActive(true);
-
-        this.animatorInimigo = GetComponent<Animator>();
-        this.rigidbodyInimigo = GetComponent<Rigidbody>();
-
-        this.pontuacao = GameObject.FindWithTag(Constantes.TAG_PONTUACAO).GetComponent<Pontuacao>();
+        CriarZumbiAleatorio();
     }
 
     // Update is called once per frame
@@ -50,21 +44,17 @@ public class ControlaInimigo : MonoBehaviour
         Vector3 direcao = this.Jogador.transform.position - transform.position;
         if (distancia < RaioVisao){ 
 
-            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
-            this.rigidbodyInimigo.MoveRotation (novaRotacao);
+            this.movimentoPersonagem.Rotacionar(direcao);
 
             if (distancia > PararDistanciaPersonagem)
             {
-                this.rigidbodyInimigo
-                    .MovePosition(this.rigidbodyInimigo.position +
-                    (direcao.normalized * Velocidade * Time.deltaTime));
-
-                this.animatorInimigo.SetBool(Constantes.ANIMACAO_ATACANDO, false);
+                this.movimentoPersonagem.Movimentar(direcao.normalized,this.Velocidade);
+                this.animacaoPersonagem.Atacar(false);
             }
             else
             {
                 // atacar
-                this.animatorInimigo.SetBool(Constantes.ANIMACAO_ATACANDO, true);
+                this.animacaoPersonagem.Atacar(true);
             }
         }
     }
@@ -79,5 +69,10 @@ public class ControlaInimigo : MonoBehaviour
     {
         this.pontuacao.IncreaseScore(1f);
         ControlaAudio.instancia.PlayOneShot(this.SomMorte);
+    }
+
+    private void CriarZumbiAleatorio() {
+        int randomZombieSkin = Random.Range(1, 28);
+        transform.GetChild(randomZombieSkin).gameObject.SetActive(true);
     }
 }
--- Assets/Scripts/ControlaJogador.cs
+++ Assets/Scripts/ControlaJogador.cs
@@ -21,13 +21,11 @@ public class ControlaJogador : MonoBehaviour
 
     public AudioClip SomDano;
 
-    private static string MOVENDO_STRING = "Movendo";
-
     private Vector3 direcao;
 
-    private Rigidbody rigidbodyJogador;
+    private AnimacaoPersonagem animacaoPersonagem;
 
-    private Animator animatorJogador;
+    private MovimentoJogador movimentoJogador;
 
     private float vertical;
 
@@ -37,8 +35,8 @@ public class ControlaJogador : MonoBehaviour
     {
         Time.timeScale = 1;
         TextoGameOver.SetActive(false);
-        this.rigidbodyJogador = GetComponent<Rigidbody>();
-        this.animatorJogador = GetComponent<Animator>();
+        this.animacaoPersonagem = GetComponent<AnimacaoPersonagem>();
+        this.movimentoJogador = GetComponent<MovimentoJogador>();
     }
 
     void Update()
@@ -48,14 +46,7 @@ public class ControlaJogador : MonoBehaviour
 
         direcao = new Vector3(eixoX, 0, eixoZ);
 
-        if (direcao != Vector3.zero)
-        {
-            this.animatorJogador.SetBool(MOVENDO_STRING, true);
-        }
-        else
-        {
-            this.animatorJogador.SetBool(MOVENDO_STRING, false);
-        }
+        animacaoPersonagem.Mover(direcao.magnitude);
 
         if (!Vivo && Input.GetButtonDown("Fire1"))
         {
@@ -65,124 +56,16 @@ public class ControlaJogador : MonoBehaviour
 
     private void FixedUpdate()
     {
-        this.movePlayer();
+        this.movimentoJogador.Movimentar(this.direcao, this.Velocidade);
 
-        /* this
-            .rigidbodyJogador
-            .MovePosition(this.rigidbodyJogador.position +
-            (direcao * Velocidade * Time.deltaTime)); */
-        Ray raio = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(raio.origin, raio.direction * 100, Color.red);
-        RaycastHit impacto;
-
-        if (Physics.Raycast(raio, out impacto, 100, MascaraChao))
-        {
-            Vector3 posicaoMiraJogador = impacto.point - transform.position;
-            posicaoMiraJogador.y = transform.position.y;
-            // Debug.Log("impacto: "+impacto.point);
-            // Debug.Log("Mira: "+posicaoMiraJogador);
-            Quaternion novaRotacao = Quaternion.LookRotation(posicaoMiraJogador);
-            
-            this.rigidbodyJogador.MoveRotation(novaRotacao);
-            // this.rigidbodyJogador.AddTorque(posicaoMiraJogador);
-            // if (novaRotacao.y != transform.rotation.y && novaRotacao.w != transform.rotation.w) {
-            //     this.rigidbodyJogador.AddRelativeTorque(0,novaRotacao.y*0.0025f,0);
-            //     Debug.Log("jogador: "+ transform.rotation);
-            //     Debug.Log("novaRotacao: "+ novaRotacao);
-            //     Debug.Log("novaRotacao: "+ novaRotacao.x);
-            //     Debug.Log("novaRotacao: "+ novaRotacao.y);
-            //     Debug.Log("novaRotacao: "+ novaRotacao.z);
-            //     Debug.Log("novaRotacao: "+ novaRotacao.w);
-            // }
-        }
-    }
-
-    private void movePlayer()
-    {
-        if (direcao != Vector3.zero)
-        {
-            float velocidade = this.Velocidade;
-            float eixoX = 0;
-            float eixoZ = 0;
-
-            float moveForwardX = transform.forward.x;
-            float moveForwardZ = transform.forward.z;
-            if (this.direcao.x != 0)
-            {
-                if (this.direcao.z == 0)
-                {
-                    velocidade *= 0.85f;
-                    if (moveForwardX > 0.5 || moveForwardX < -0.5)
-                    {
-                        int multDirecao = CalcUtils.GetDirecao(this.direcao.x);
-                        eixoX = moveForwardZ * multDirecao;
-                        eixoZ = moveForwardX * -1 * multDirecao;
-                    }
-                    else
-                    {
-                        int multDirecao =
-                            CalcUtils.GetDirecaoInvertida(this.direcao.x);
-                        eixoX = moveForwardZ * -1 * multDirecao;
-                        eixoZ = moveForwardX * multDirecao;
-                    }
-                }
-                else
-                {
-                    int multDirecaoX = CalcUtils.GetDirecao(this.direcao.x);
-                    int multDirecaoZ = CalcUtils.GetDirecao(this.direcao.z);
-                    float direcaoXPositivo =
-                        CalcUtils.GetValorPositivo(this.direcao.x);
-                    float direcaoZPositivo =
-                        CalcUtils.GetValorPositivo(this.direcao.z);
-                    float moveForwardXPositivo =
-                        CalcUtils.GetValorPositivo(moveForwardX);
-                    float moveForwardZPositivo =
-                        CalcUtils.GetValorPositivo(moveForwardZ);
-                    velocidade *= 0.3f;
-                    eixoX =
-                        (moveForwardXPositivo + direcaoXPositivo) *
-                        multDirecaoX;
-                    eixoZ =
-                        (moveForwardZPositivo + direcaoZPositivo) *
-                        multDirecaoZ;
-                }
-            }
-            else
-            {
-                int multDirecao = CalcUtils.GetDirecao(this.direcao.z);
-                if (this.direcao.z < 0)
-                {
-                    velocidade *= 0.85f;
-                }
-                eixoX = moveForwardX * multDirecao;
-                eixoZ = moveForwardZ * multDirecao;
-            }
-
-            /* if (this.direcao.z < 0) {
-                eixoZ *= -1;
-                eixoX *= -1;
-            } */
-            Vector3 vector = new Vector3(eixoX, 0, eixoZ);
-
-            // Debug.Log("Forward: " + transform.forward);
-            // Debug.Log("direcao: " + direcao);
-            // Debug.Log("vector: " + vector);
-            Vector3 angularVelocity = new Vector3(1, 1, 1);
-            this.rigidbodyJogador.velocity = vector * velocidade;
-            //Debug.Log("velocity: " + this.rigidbodyJogador.velocity);
-        }
-        else
-        {
-            this.rigidbodyJogador.velocity = Vector3.zero;
-            this.rigidbodyJogador.angularVelocity = Vector3.zero;
-        }
+        this.movimentoJogador.RotacionarJogador(this.MascaraChao);
     }
 
     public void TomarDano(int valorDano)
     {
         this.Vida -= valorDano;
         controlaInterface.AtualizarSliderVidaJogador();
-        ControlaAudio.instancia.PlayOneShot(this.SomDano); 
+        ControlaAudio.instancia.PlayOneShot(this.SomDano);
         verificaMorte();
     }
 
